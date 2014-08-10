42c98902996e7a29cfe06053cc5de08f67168ed9
Rohan Patil <rohanapatil88@gmail.com>
2014-08-09 20:05:45 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -5,7 +5,7 @@ household_power_consumption$Global_active_power <- as.numeric(as.character(house
 household_power_consumption$Date <- as.Date(household_power_consumption$Date,format ='%d/%m/%Y') #Converting Factor to date 
 power_set <- household_power_consumption[household_power_consumption$Date==("2007-02-01") |household_power_consumption$Date==("2007-02-02"),] #creating a subset with given conditions
 power_set$Date <- paste(power_set$Date,power_set$Time,sep = " ") #Concatenation
-power_set$Date <- strptime(power_set$Date,"%Y-%m-%d %H:%M:%S")
+power_set$Date <- strptime(power_set$Date,"%Y-%m-%d %H:%M:%S") #Formating Date
 power_set$Sub_metering_1 <- as.numeric(as.character(power_set$Sub_metering_1))# Converting Factor into numeric
 power_set$Sub_metering_2 <- as.numeric(as.character(power_set$Sub_metering_2))# Converting Factor into numeric
 power_set$Sub_metering_3 <- as.numeric(as.character(power_set$Sub_metering_3))# Converting Factor into numeric
