9b942c37ec7f0b308ee7357c02c2f55f0ac617dd
Rohan Patil <rohanapatil88@gmail.com>
2014-08-09 20:04:49 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -5,7 +5,7 @@ household_power_consumption$Global_active_power <- as.numeric(as.character(house
 household_power_consumption$Date <- as.Date(household_power_consumption$Date,format ='%d/%m/%Y') #Converting Factor to date 
 power_set <- household_power_consumption[household_power_consumption$Date==("2007-02-01") |household_power_consumption$Date==("2007-02-02"),] #creating a subset with given conditions
 power_set$Date <- paste(power_set$Date,power_set$Time,sep = " ") #Concatenation
-power_set$Date <- strptime(power_set$Date,"%Y-%m-%d %H:%M:%S")
+power_set$Date <- strptime(power_set$Date,"%Y-%m-%d %H:%M:%S")#Formating Date
 with(power_set,plot(power_set$Date,Global_active_power,xlab="",ylab="Global Active Power",type="n")) #Ploting 
 with(power_set,lines(power_set$Date,power_set$Global_active_power))
 dev.copy(png,"plot2.png",width=480,height=480,units='px') # Creating png file
